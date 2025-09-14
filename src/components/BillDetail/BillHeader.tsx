9ebc878e4045bebcbb684eda526cbacbf0bb8b55
Mikaal Naik <matthewnaik@gmail.com>
2025-09-14 15:01:32 -0400

    better mobile bill page

--- src/components/BillDetail/BillHeader.tsx
+++ src/components/BillDetail/BillHeader.tsx
@@ -7,7 +7,7 @@ interface BillHeaderProps {
 export function BillHeader({ bill }: BillHeaderProps) {
   return (
     <header className="pb-4">
-      <h1 className="  mb-6 text-[48px] leading-14 font-bold">
+      <h1 className="mb-6 md:text-[48px] text-[32px] leading-8 md:leading-14 font-bold">
         {bill.short_title}
       </h1>
       <p className="mt-2 text-sm text-[ w-1/2">
