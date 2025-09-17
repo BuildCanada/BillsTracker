6ef2832a430fe6910b054c54dec990464b0a7462
xrendan <brendan@brendansamek.com>
2025-09-17 16:16:31 -0600

    Update the base url to /bills

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -58,7 +58,7 @@ export default async function BillDetail({ params }: Params) {
           ← Back to bills
         </Link>
         {session?.user && (
-          <Link href={`/bills/${params.id}/edit`} className="ml-4 text-sm underline">
+          <Link href={`/${params.id}/edit`} className="ml-4 text-sm underline">
             Edit
           </Link>
         )}
