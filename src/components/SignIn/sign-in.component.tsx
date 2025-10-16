3bc22aacc27a1dcceac8deeefe954c2b72ee56d2
Shawn Price <hi@shawnprice.com>
2025-10-15 18:26:57 -0700

    Simpler Admin link

--- src/components/SignIn/sign-in.component.tsx
+++ src/components/SignIn/sign-in.component.tsx
@@ -1,18 +1,10 @@
-"use client";
-import { signIn } from "next-auth/react";
-import { BASE_PATH } from "@/utils/basePath";
+import Link from "next/link";
 
 export const SignIn = () => {
-  const handleSignInClick = () => {
-    signIn("google", { callbackUrl: BASE_PATH || "/" });
-  };
   return (
     <div>
-      <p
-        className="text-xs text-[var(--muted-foreground)] cursor-pointer"
-        onClick={handleSignInClick}
-      >
-        Admin
+      <p className="text-xs text-[var(--muted-foreground)] cursor-pointer">
+        <Link href={"/sign-in"}>Admin</Link>
       </p>
     </div>
   );
